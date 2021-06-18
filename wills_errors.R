438c1291a93d46e4c832f4d8c5488a4a61015ca6
Andrew Leach <leach.andrew@gmail.com>
2021-06-18 09:06:33 -0600

    Update wills_errors.R

--- wills_errors.R
+++ wills_errors.R
@@ -14,20 +14,19 @@ data_set<-forecast_data %>% filter(time>=ymd("2010-01-1"))%>%assign_date_time_da
 
 #adjust for both data and forecasts for observeables
 
-price_model <- lm(actual_posted_pool_price ~factor(he) + factor(day)+factor(month)+factor(year)+factor(on_peak), data = data_set)
-tidy(price_model)
+price_model <- lm(actual_posted_pool_price ~ poly(day_ahead_forecasted_ail,3) +factor(he)*month_fac + factor(year)+factor(on_peak)+poly(cdd_YEG,2)+poly(hdd_YEG,2), data = data_set)
+coeffs<-tidy(price_model)
 resid<-augment(price_model)
 
-resid<-resid%>% mutate(resid=actual_posted_pool_price-.fitted,
-                       date=ymd_h(paste(`factor(year)`,"-",`factor(month)`,"-",`factor(day)`," ",`factor(he)`,sep="")))
+resid<-resid%>% mutate(resid=actual_posted_pool_price-.fitted)
 
+#x_reg<-data_set %>% select(day_ahead_forecasted_ail,year,month_fac,he,hdd_YEG,cdd_YEG)%>%
+#  mutate(load2=day_ahead_forecasted_ail^2,year=factor(year),hdd2=hdd_YEG^2,cdd2=cdd_YEG^2)
+#x_reg<-fastDummies::dummy_cols(x_reg)%>% select(-year,-month_fac,-year_2010,-month_fac_Jan,-he_01,-he)
 
-x_reg<-data_set %>% select(day_ahead_forecasted_ail,year,month_fac,he,hdd_YEG,cdd_YEG)%>%
-  mutate(load2=day_ahead_forecasted_ail^2,year=factor(year),hdd2=hdd_YEG^2,cdd2=cdd_YEG^2)
-x_reg<-fastDummies::dummy_cols(x_reg)%>% select(-year,-month_fac,-year_2010,-month_fac_Jan,-he_01,-he)
+#ar_fit<-arima(data_set$actual_posted_pool_price,order = c(2,0,0),xreg =x_reg)
 
-ar_fit<-arima(data_set$actual_posted_pool_price,order = c(2,0,0),xreg =x_reg)
-
+ar_fit<-arima(resid$resid,order = c(2,0,0))
 tidy(ar_fit)
 glance(ar_fit)
 
