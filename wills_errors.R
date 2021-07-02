cd43dae43a154cb1a98a1b375c105ed094ab76ea
WillNoel <wnoel@ualberta.ca>
2021-07-02 14:44:05 -0600

    Added more reduced models

--- wills_errors.R
+++ wills_errors.R
@@ -39,50 +39,81 @@ lm_data <- data_set %>%
 # Build a full model then test if variables can be removed using partial F test
 full_model <- lm(actual_posted_pool_price~., data = lm_data)
 anova0 <- anova(full_model)
+sum0 <- summary(full_model)
 
 # Reduced model 1: remove 1 hour lag of pool price
 red_model1 <- lm(actual_posted_pool_price ~. -lag1, data = lm_data)
 anova1 <- anova(red_model1,full_model)
+sum1 <- summary(red_model1)
 
 # Reduced model 2: remove 2 hour lag of pool price
 red_model2 <- lm(actual_posted_pool_price ~. -lag2, data = lm_data)
 anova2 <- anova(red_model2,full_model)
+sum2 <- summary(red_model2)
 
 # Reduced model 3: remove day ahead forecasted ail
 red_model3 <- lm(actual_posted_pool_price ~. -day_ahead_forecasted_ail, data = lm_data)
 anova3 <- anova(red_model3,full_model)
+sum3 <- summary(red_model3)
 
 # Reduced model 4: remove stat holiday factor
 red_model4 <- lm(actual_posted_pool_price ~. -stat, data = lm_data)
 anova4 <- anova(red_model4,full_model)
+sum4 <- summary(red_model4)
 
 # Reduced model 5: remove on/off peak factor
 red_model5 <- lm(actual_posted_pool_price ~. -on_peak, data = lm_data)
 anova5 <- anova(red_model5,full_model)
+sum5 <- summary(red_model5)
 
 # Reduced model 6: remove cooling degree days
 red_model6 <- lm(actual_posted_pool_price ~. -cdd_YYC, data = lm_data)
 anova6 <- anova(red_model6,full_model)
+sum6 <- summary(red_model6)
 
 # Reduced model 7: remove heating degree days
 red_model7 <- lm(actual_posted_pool_price ~. -hdd_YYC, data = lm_data)
 anova7 <- anova(red_model7,full_model)
+sum7 <- summary(red_model7)
 
 # Reduced model 8: remove year
 red_model8 <- lm(actual_posted_pool_price ~. -year, data = lm_data)
 anova8 <- anova(red_model8,full_model)
+sum8 <- summary(red_model8)
 
 # Reduced model 9: remove month-hour interaction
 red_model9 <- lm(actual_posted_pool_price ~. -month_he, data = lm_data)
 anova9 <- anova(red_model9,full_model)
+sum9 <- summary(red_model9)
 
 # Reduced model 10: remove weekday/weekend-hour interaction
 red_model10 <- lm(actual_posted_pool_price ~. -weekday_he, data = lm_data)
 anova10 <- anova(red_model10,full_model)
+sum10 <- summary(red_model10)
 
+# Reduced model 11: remove stat holiday & on/off peak
+red_model11 <- lm(actual_posted_pool_price ~. -on_peak -stat, data = lm_data)
+anova11 <- anova(red_model11,full_model)
+sum11 <- summary(red_model11)
 
-coeffs<-tidy(full_model)
-resid<-augment(full_model)
+# Reduced model 12: remove stat holiday, on/off peak, & lag2
+red_model12 <- lm(actual_posted_pool_price ~. -on_peak -stat -lag2, data = lm_data)
+anova12 <- anova(red_model12,full_model)
+sum12 <- summary(red_model12)
+
+# Reduced model 13: remove stat holiday, on/off peak, lag2, & day ahead forecasted ail
+red_model13 <- lm(actual_posted_pool_price ~. -on_peak -stat -lag2 -day_ahead_forecasted_ail, data = lm_data)
+anova13 <- anova(red_model13,full_model)
+sum13 <- summary(red_model13)
+
+# Reduced model 14: remove stat holiday, on/off peak, lag2, day ahead forecasted ail, weekday/weekend - he interaction
+red_model14 <- lm(actual_posted_pool_price ~. -on_peak -stat -lag2 -day_ahead_forecasted_ail -weekday_he, data = lm_data)
+anova14 <- anova(red_model14,full_model)
+sum14 <- summary(red_model14)
+
+# Choose reduced model 14
+coeffs<-tidy(red_model14)
+resid<-augment(red_model14)
 
 
 #resid<-resid%>% mutate(resid=actual_posted_pool_price-.fitted)
@@ -103,7 +134,7 @@ resid<-augment(full_model)
 
 #ts.sim<-resid$.fitted[1:1000]+arima.sim(list(ar = c(0.00000000000001,0.000000000000001)),n=1000,sd = ar_glance$sigma)
 
-ts_test<-as_tibble(rnorm(nrow(resid)+500,0,sd=resid$.sigma[1])) #use 500 iterations to seed the processs
+ts_test<-as_tibble(rnorm(nrow(resid)+500,0,sd=sum14$sigma)) #use 500 iterations to seed the processs
 
 ts_test<-tail(ts_test,nrow(resid)) #trim those 500 seed values
 ts_test<-ts_test %>% mutate(ar_val=resid$.fitted+value,
@@ -111,5 +142,12 @@ ts_test<-ts_test %>% mutate(ar_val=resid$.fitted+value,
                             ar_val=pmin(ar_val,1000)
                             ) #add in the fitted price values
 
+comb.ts <- cbind(resid$actual_posted_pool_price,resid$.fitted,ts_test$ar_val)
 
-ts.plot(ts_test$ar_val)
+ts.plot(comb.ts, gpars = list(col = c("black","green","red")))
+legend("topleft",
+       bty="n",
+       lty=c(1, 1, 1, 1),
+       lwd=c(2, 2, 2, 2),
+       c("actual posted price","linear model","linear model + ARIMA error"),
+       col = c("black","green","red"))
